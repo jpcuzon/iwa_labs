7bd0753c7547d618f4afecf243d46664a2d90ef9
jpcuzon <jpcuzon7@gmail.com>
2022-03-25 11:14:31 +0000

    MonogDB Data

--- model/user.js
+++ model/user.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, unique: true, lowercase: true},
+
+    email: { type: String, unique : true, lowercase: true},
     password: String,
     username: String,
     gender: {
         type: String,
-        enum: ['MALE', 'FEMALE']
+        enum: ['Male', 'Female']
     },
     phone: Number
 })
